06feb26725f5b8f01f31e747d473d8b42a863cc2
fhzh123 <fhzh@naver.com>
2019-12-24 19:45:49 +0900

    fix

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-python3 train.py --src_rev_usage=False --n_layers=6 --batch_size=400 --num_epoch=5 --data=metr
-python3 train.py --src_rev_usage=True --n_layers=6 --batch_size=400 --num_epoch=5 --data=metr
-python3 train.py --src_rev_usage=True --n_layers=12 --batch_size=300 --num_epoch=5 --data=metr
-python3 train.py --src_rev_usage=False --n_layers=12 --batch_size=300 --num_epoch=5 --data=metr
+python3 train.py --src_rev_usage=False --n_layers=12 --batch_size=200 --num_epoch=6 --data=pems --repeat_input=True
+python3 train.py --src_rev_usage=False --n_layers=12 --batch_size=200 --num_epoch=6 --data=pems --repeat_input=True --src_rev_usage=False
+python3 train.py --src_rev_usage=False --n_layers=24 --batch_size=100 --num_epoch=6 --data=pems --repeat_input=True
